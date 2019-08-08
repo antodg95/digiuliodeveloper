8d3dc334a396b910699111ae2ce14583737ab2d4
antodg95 <antodg95@gmail.com>
2019-08-08 15:39:42 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("navbar").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("navbar").style.width = "0%";
+}
+
+document.getElementById('overlay-navbar').addEventListener("click", closeNav);
